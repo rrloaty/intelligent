fe6458cc974c9619c92ae3f3c91e9f4f8924301e
rrloaty <realloaded93@gmail.com>
2025-08-09 19:19:58 +0100

    Update premiumlist.js

--- premiumlist.js
+++ premiumlist.js
@@ -2,7 +2,7 @@
 // Array of Telegram user IDs allowed premium access
 
 const premiumUsers = [
-  123456789,  // Replace with actual Telegram user IDs
+  6976365864,  // Replace with actual Telegram user IDs
   987654321,
 6976365864,
   // Add more IDs here
